d9acc0694d784ab47fcea8e10bfe7a6b01a7cb5f
Polarts <davidklein.4496@gmail.com>
2021-02-15 21:41:40 +0200

    Extended menu and removed redundancies

--- skeleton/index.js
+++ skeleton/index.js
@@ -6,21 +6,34 @@ const buttonSize = button.getBoundingClientRect();
 const mc = new Hammer(button);
 mc.get('pan').set({direction: Hammer.DIRECTION_ALL});
 mc.get('press').set({time: 0});
-console.log(mc);
+
+var panDirection = 'none';
 
 mc.on("press", function(e) {
     button.style.transition = "none";
     container.classList.add('active');
 });
 mc.on("panleft panright", function(e) {
-if (Math.abs(e.deltaX) <= buttonSize.width) {
-    button.style.transform=`translateX(${e.deltaX}px)`;
-}
+    if (panDirection !== 'vertical') {
+        panDirection = 'horizontal';
+        if (Math.abs(e.deltaX) <= buttonSize.width) {
+            button.style.transform=`translateX(${e.deltaX}px)`;
+        }
+    }   
+});
+mc.on("panup pandown", function(e) {
+    if (panDirection !== 'horizontal') {
+        panDirection = 'vertical'
+        if (Math.abs(e.deltaY) <= buttonSize.height) {
+            button.style.transform=`translateY(${e.deltaY}px)`;
+        }
+    }
 });
 mc.on("pressup", function(e) {
     returnToSender();
 });
 mc.on("panend", function(e) {
+    panDirection = 'none';
     returnToSender();
 });
 
