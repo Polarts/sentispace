8e988d824059729f677ef0e05cb2d47ae1e7b5b2
Polarts <davidklein.4496@gmail.com>
2021-03-09 21:06:20 +0200

    Added routing and started implementing components

--- src/ViewModels/ActivityFormViewModel.ts
+++ src/ViewModels/ActivityFormViewModel.ts
@@ -1,12 +1,59 @@
-import {observable} from 'mobx';
+import {IObservableArray, observable} from 'mobx';
 import Activity from '../Models/Activity';
+import FeelingsEnum from '../Models/FeelingsEnum';
 import ActivitiesStore from '../Stores/ActivitiesStore';
 
 export default class ActivityFormViewModel {
 
-    constructor(
-        public model: Activity,
-        public store: ActivitiesStore
-    ) {}
+    //#region properties
+    private id?: string;
 
+    @observable
+    public title: string;
+
+    @observable
+    public description: string;
+
+    @observable
+    public feeling: FeelingsEnum;
+
+    @observable
+    public time: Date;
+
+    @observable
+    public tags: IObservableArray<string>;
+    //#endregion
+
+    constructor(
+        private model: Activity,
+        private store: ActivitiesStore
+    ) {
+        this.id = model.id;
+        this.title = model.title;
+        this.description = model.description;
+        this.feeling = model.feeling;
+        this.time = model.time;
+        this.tags = observable.array(model.tags);
+    }
+
+    public save() {
+        if(this.id) {
+            this.store.create(new Activity(
+                this.title,
+                this.description,
+                this.feeling,
+                this.time,
+                this.tags.slice()
+            ));
+        } else {
+            this.store.update(new Activity(
+                this.title,
+                this.description,
+                this.feeling,
+                this.time,
+                this.tags.slice(),
+                this.id
+            ));
+        }
+    }
 }
