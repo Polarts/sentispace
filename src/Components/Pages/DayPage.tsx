c05d03282239d1d94bc80a2e05ea1efd909d2706
Polarts <davidklein.4496@gmail.com>
2021-03-12 01:37:03 +0200

    Added working mock and data bindings, added nav header

--- src/Components/Pages/DayPage.tsx
+++ src/Components/Pages/DayPage.tsx
@@ -1,31 +1,16 @@
 import React from 'react';
 import { observer } from 'mobx-react';
-import Activity from '../../Models/Activity';
-import FeelingsEnum from '../../Models/FeelingsEnum';
 import ActivityItem from '../ActivityItem';
-
-type DayPageProps = {
-
-};
+import ActivitiesStore from '../../Stores/ActivitiesStore';
 
 export default observer(
-    (props: DayPageProps) => {
+    () => {
 
-        const activities = [];
-        for (let i=0; i<5; i++) {
-            activities.push(new Activity(
-                "Lorem ipsum dolor sit amet!",
-                "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Qui cupiditate similique, repellat alias veniam reprehenderit debitis maiores, architecto modi repellendus delectus saepe assumenda vero obcaecati adipisci nisi eius fugiat porro.",
-                FeelingsEnum.ok,
-                new Date(),
-                [],
-                String(i)
-            ));
-        }
+        const store = ActivitiesStore.instance;
 
         return (
             <main>
-                {activities.map(act => <ActivityItem model={act}/> )}
+                {store.activities.map(act => <ActivityItem {...act} key={act.id}/> )}
             </main>
         );
     }
