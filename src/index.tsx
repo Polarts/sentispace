c5c4f24be60f23a77e967b632a2482c81ec83ed4
Polarts <polarbear.oc@gmail.com>
2021-06-19 19:46:14 +0300

    Changed position of service worker init

--- src/index.tsx
+++ src/index.tsx
@@ -10,8 +10,6 @@ import './Styles/imports/reset.css';
 import './Styles/imports/fontawesome.css';
 import './Styles/main.css';
 
-registerServiceWorker();
-
 document.addEventListener('contextmenu', e => {
   e.preventDefault();
   return false;
@@ -24,7 +22,9 @@ ReactDOM.render(
     </BrowserRouter>
   </React.StrictMode>,
   document.getElementById('root')
-);
+  );
+  
+registerServiceWorker();
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
