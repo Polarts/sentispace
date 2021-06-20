ce92f98f327e6adf2df37330391259cb24b9198d
Polarts <polarbear.oc@gmail.com>
2021-06-20 18:59:11 +0300

    Changed SW to CRA's default and now it works

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter as Router } from 'react-router-dom';
 
-import registerServiceWorker from "./serviceWorker";
+import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 import reportWebVitals from './reportWebVitals';
 
 import './Styles/imports/reset.css';
@@ -17,14 +17,17 @@ document.addEventListener('contextmenu', e => {
 
 ReactDOM.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <Router>
       <App /> 
-    </BrowserRouter>
+    </Router>
   </React.StrictMode>,
   document.getElementById('root')
   );
   
-registerServiceWorker();
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://cra.link/PWA
+serviceWorkerRegistration.register();
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
