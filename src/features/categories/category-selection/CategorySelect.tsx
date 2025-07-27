7df585af4556ebc04d675f26781f41b966e6e9cf
Ziv89 <46285020+Ziv89@users.noreply.github.com>
2025-07-27 15:18:10 +0300

    Update CategorySelect.tsx
    1.Removed  unnesseccery if condition,
    2.document being formated.
    3.Removed   code block duplication.

--- src/features/categories/category-selection/CategorySelect.tsx
+++ src/features/categories/category-selection/CategorySelect.tsx
@@ -9,6 +9,7 @@ import CategoryBadge from './category-badge/CategoryBadge';
 import CategorySelectionModal from './CategorySelectionModal';
 
 const cx = classNames.bind(classes);
+export type CategorySelectionFunction = (categoryId: IndexableType, operation: 'add' | 'remove') => void;
 
 interface SelectProps {
   label: string;
@@ -16,7 +17,7 @@ interface SelectProps {
   iconKey?: IconKeyType;
   categoryIds: IndexableType[];
   onCategoriesChange: (categories: IndexableType[]) => void;
-   readOnly?: boolean;
+  readOnly?: boolean;
 }
 
 const CategorySelect = ({
@@ -33,69 +34,61 @@ const CategorySelect = ({
     setIsOpen((prev) => !prev);
   };
 
-const handleCategorySelection = (categoryId: IndexableType, operation: 'add' | 'remove') => {
-  if (readOnly && operation === 'remove') return;
+  const handleCategorySelection = (categoryId: IndexableType, operation: 'add' | 'remove') => {
 
-  switch (operation) {
-    case 'add':
-      setSelectedCategories([...categoryIds, categoryId]);
-      break;
-    case 'remove':
-      setSelectedCategories(categoryIds.filter((id) => id !== categoryId));
-      break;
-  }
-};
+    switch (operation) {
+      case 'add':
+        setSelectedCategories([...categoryIds, categoryId]);
+        break;
+      case 'remove':
+        setSelectedCategories(categoryIds.filter((id) => id !== categoryId));
+        break;
+    }
+  };
 
   return (
     <>
-   <div className={classes.categorySelect}>
-  <span className={classes.label}>{label}</span>
-  <div
-    tabIndex={0}
-    className={cx({ select: true, isOpen })}
-    onClick={handleSelectToggle}
-  >
-    <div className={classes.selectIcon}>
-      <Hash />
-    </div>
-    <div className={classes.selectedCategoriesWrapper}>
-      {categoryIds.length === 0 ? (
-        <span className={classes.placeholder}>{placeholder}</span>
-      ) : (
-        <div className={classes.flexContainer}>
-          {categories
-            ?.filter(({ id }) => categoryIds.includes(id))
-            .map((cat) => (
-              <div className={classes.flexContainerItem} key={cat.id.toString()}>
-               <CategoryBadge
-  {...cat}
-  onClick={!readOnly ? () => handleCategorySelection(cat.id, 'remove') : undefined}
-/>
+      <div className={classes.categorySelect}>
+        <span className={classes.label}>{label}</span>
+        <div
+          tabIndex={0}
+          className={cx({ select: true, isOpen })}
+          onClick={handleSelectToggle}
+        >
+          <div className={classes.selectIcon}>
+            <Hash />
+          </div>
+          <div className={classes.selectedCategoriesWrapper}>
+            {categoryIds.length === 0 ? (
+              <span className={classes.placeholder}>{placeholder}</span>
+            ) : (
+              <div className={classes.flexContainer}>
+                {categories
+                  ?.filter(({ id }) => categoryIds.includes(id))
+                  .map((cat) => (
+                    <div className={classes.flexContainerItem} key={cat.id.toString()}>
+                      <CategoryBadge
+                        {...cat}
+                        onClick={!readOnly ? () => handleCategorySelection(cat.id, 'remove') : undefined}
+                      />
+                    </div>
+                  ))}
               </div>
-            ))}
+            )}
+          </div>
+          <div className={classes.toggleSelect}>
+            <CaretRight />
+          </div>
         </div>
-      )}
-    </div>
-    <div className={classes.toggleSelect}>
-      <CaretRight />
-    </div>
-  </div>
 
-  {isOpen && (
-    <CategorySelectionModal
-      onClose={handleSelectToggle}
-      categoryIds={categoryIds}
-      onCategoriesChange={setSelectedCategories}
-    />
-  )}
-</div>
-      {isOpen && (
-        <CategorySelectionModal
-          onClose={handleSelectToggle}
-          categoryIds={categoryIds}
-          onCategoriesChange={setSelectedCategories}
-        />
-      )}
+        {isOpen && (
+          <CategorySelectionModal
+            onClose={handleSelectToggle}
+            categoryIds={categoryIds}
+            onCategoriesChange={setSelectedCategories}
+          />
+        )}
+      </div>
     </>
   );
 };
